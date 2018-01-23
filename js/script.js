7ec9354b31bb0864dbbf94000e240a89288cb6d3
tcortin <tcortin@gmail.com>
2018-01-23 01:19:05 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,68 @@
+jQuery(function($){
+    // external js: isotope.pkgd.js
+
+// init Isotope
+var portfolio = $('#portfolio');
+var grid = $('.grid');
+var $grid = $('.grid').isotope({
+    itemSelector: '.element-item',
+    layoutMode: 'masonry',
+    transitionDuration: '0.8s',
+    masonry: {
+        columnWidth: 6,
+    }
+  });
+
+  // filter functions
+  var filterFns = {
+  };
+
+  // bind filter button click
+  $('.filters-button-group').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    // use filterFn if matches value
+    filterValue = filterFns[ filterValue ] || filterValue;
+    $grid.isotope({ filter: filterValue });
+  });
+
+  // change is-checked class on buttons
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
+  });
+
+// modify the location.hash regarding the category
+  $('button').click(function(e){
+    var cls = $(this).attr('name');
+    location.hash = cls;
+    e.preventDefault();
+  });
+
+// change class/size on click
+  grid.children().click( function() {
+    grid.find('.large').removeClass('large');
+    $(this).toggleClass('large');
+    grid.isotope('layout');
+  });
+
+  if(location.hash != ''){
+    $('button[name="'+location.hash+'"]').trigger('click');
+  }
+
+// animation of footer-icon
+  $('.footer-icon').click(function(e){
+    e.preventDefault();
+    $this=$(this);
+    if($this.hasClass('is-opened')){
+      $this.addClass('is-closed').removeClass('is-opened');
+      $this.parent().addClass('is-closed').removeClass('is-opened');
+    }else{
+      $this.removeClass('is-closed').addClass('is-opened');
+      $this.parent().removeClass('is-closed').addClass('is-opened');
+    }
+  })
+  
+})
